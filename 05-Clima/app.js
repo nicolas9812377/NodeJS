fedd90e8991aac28bfd658ecbdd1cec9912ed961
unknown <nicolascarrasco98@hotmail.com>
2020-06-16 16:55:15 -0500

    Version Clima con Humedad y Presion

--- 05-Clima/app.js
+++ 05-Clima/app.js
@@ -4,16 +4,21 @@ const argv = require('yargs').options({
         alias: 'c',
         descripcion: 'Nombre de la Ciudad para obtener el clima',
         demand: true
+    },
+    opciones: {
+        alias: 'o',
+        descripcion: 'Opcion para escoger Humedad (H) o Presion Atmosferica (P)',
     }
 }).argv;
 
-const getInfor = async(ciudad) => {
+const getInfor = async(ciudad, opciones = '') => {
     try {
-        const temp = await clima.getClima(ciudad);
-        return `El clima de la ciudad ${   ciudad  } es de ${  temp    }`;
+        const rs = await clima.getClima(ciudad);
+        let op = (opciones.toLowerCase() === 'h') ? `con una Humedad de ${rs.humidity}` : (opciones.toLowerCase() === 'p') ? `con una Presion Atomosferica de ${rs.pressure}` : '';
+        return `El clima de la ciudad ${   ciudad  } es de ${  rs.temp    } ${op}`;
     } catch (e) {
         return `No se pudo obtener el clima de la ciudad ${ ciudad  }`;
     }
 }
 
-getInfor(argv.ciudad).then(console.log).catch(console.log);
+getInfor(argv.ciudad, argv.opciones).then(console.log).catch(console.log);
